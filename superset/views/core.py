7493092e10f674bf6453c780b39517794a45baa4
axelblak3333 <axelblack2282@gmail.com>
2026-05-19 11:09:45 +0500

    Фикс перевода

--- superset/views/core.py
+++ superset/views/core.py
@@ -902,7 +902,7 @@ class Superset(BaseSupersetView):
         return json_success(json.dumps(sanitize_datasource_data(datasource.data)))
 
     @event_logger.log_this
-    @has_access
+    #@has_access - блокирует язык у неавторизованных пользователей, смысла нет защищать эту ручку
     @expose("/language_pack/<lang>/")
     def language_pack(self, lang: str) -> FlaskResponse:
         # Only allow expected language formats like "en", "pt_BR", etc.

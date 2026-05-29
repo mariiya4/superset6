d73721ec792c4f4d3a6e54b8e2a22006d51ef1e2
axelblak3333 <axelblack2282@gmail.com>
2026-05-29 11:36:48 +0500

    Только ру D3

--- superset/views/base.py
+++ superset/views/base.py
@@ -564,7 +564,21 @@ def cached_common_bootstrap_data(  # pylint: disable=unused-argument
                         'дек']
         }
     else:
-        D3_TIME_FORMAT = {}
+        D3_TIME_FORMAT = {
+             "dateTime": "%x, %X",
+             "date": "%-m/%-d/%Y",
+             "time": "%-I:%M:%S %p",
+             "periods": ["AM", "PM"],
+             "days": ["Sunday", "Monday", "Tuesday", "Wednesday",
+                      "Thursday", "Friday", "Saturday"],
+             "shortDays": ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+             "months": ["January", "February", "March", "April",
+                        "May", "June", "July", "August",
+                        "September", "October", "November", "December"],
+             "shortMonths": ["Jan", "Feb", "Mar", "Apr",
+                             "May", "Jun", "Jul", "Aug",
+                             "Sep", "Oct", "Nov", "Dec"]
+         }
 
     bootstrap_data = {
         "application_root": app.config["APPLICATION_ROOT"],
